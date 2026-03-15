e96c6a03ce4bcc664f9933ce872f7eb9dbd40a50
ErtanAtalar <ertanatalar@Ertan-MacBook-Air.local>
2026-03-15 21:49:22 +0300

    Harden security settings and make admin path configurable

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,17 @@
+import os
+
 from django.contrib import admin
 from django.urls import include, path
 from django.views.i18n import set_language
 
 from core.views import portal
 
+admin_path = os.getenv("DJANGO_ADMIN_PATH", "admin/").lstrip("/")
+if not admin_path.endswith("/"):
+    admin_path += "/"
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(admin_path, admin.site.urls),
     path('i18n/setlang/', set_language, name='set_language'),
     path('', portal, name='portal'),
     path('price-demand/', include('core.urls')),
