8084bf190d4cd6fb0b77e7e48a02e356d3485d10
ErtanAtalar <ertanatalar@Ertan-MacBook-Air.local>
2026-04-12 22:18:23 +0300

    Add sitemap.xml and robots.txt routes for Search Console

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.i18n import set_language
 
-from core.views import ads_txt, portal, privacy_policy
+from core.views import ads_txt, portal, privacy_policy, robots_txt, sitemap_xml
 
 admin_path = os.getenv("DJANGO_ADMIN_PATH", "admin/").lstrip("/")
 if not admin_path.endswith("/"):
@@ -15,6 +15,8 @@ urlpatterns = [
     path('i18n/setlang/', set_language, name='set_language'),
     path("ads.txt", ads_txt, name="ads_txt"),
     path("ads.txt/", ads_txt),
+    path("sitemap.xml", sitemap_xml, name="sitemap_xml"),
+    path("robots.txt", robots_txt, name="robots_txt"),
     path("privacy/", privacy_policy, name="privacy_policy"),
     path('', portal, name='portal'),
     path('price-demand/', include('core.urls')),
